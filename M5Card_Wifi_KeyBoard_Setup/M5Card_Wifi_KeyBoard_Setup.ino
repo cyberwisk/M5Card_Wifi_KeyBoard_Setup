18a2eb01c97098a336c3af25f054cbb31e42b311
Aurélio Avanzi <cyberwisk@gmail.com>
2024-01-31 21:26:45 -0300

    Update M5Card_Wifi_KeyBoard_Setup.ino

--- M5Card_Wifi_KeyBoard_Setup/M5Card_Wifi_KeyBoard_Setup.ino
+++ M5Card_Wifi_KeyBoard_Setup/M5Card_Wifi_KeyBoard_Setup.ino
@@ -1,49 +1,26 @@
 /**
- * @file M5Cardputer_WebRadio.ino
+ * @file M5Card_Wifi_KeyBoard_Setup.ino
  * @author Aurélio Avanzi
- * @brief M5Cardputer WebRadio
+ * @brief https://github.com/cyberwisk/M5Card_Wifi_KeyBoard_Setup/tree/main/M5Card_Wifi_KeyBoard_Setup
  * @version Apha 0.3
  * @date 2024-01-30
  *
  * @Hardwares: M5Cardputer - https://docs.m5stack.com/en/core/Cardputer
- * @Platform Version: Arduino M5Stack Board Manager
  * @Dependent Librarys:
- * M5GFX: https://github.com/m5stack/M5GFX
- * EEPROM: https://github.com/m5stack/m5-docs/blob/master/docs/en/api/eeprom.md
+ * M5Cardputer: https://github.com/m5stack/M5Cardputer
+ * WiFi: https://github.com/espressif/arduino-esp32/tree/master/libraries/WiFi
+ * Preferences: https://github.com/m5stack/azure_iothub_arduino_lib_esp32/blob/master/hardware/espressif/esp32/libraries/Preferences/src/Preferences.h
  **/
 
 #include <M5Cardputer.h>
 #include <WiFi.h>
-#include <EEPROM.h>
+#include <Preferences.h>
 
-// Define os endereços de memória EEPROM para SSID e Senha
-#define SSID_ADDR 0
-#define PASS_ADDR 32
+#define NVS_SSID_KEY "wifi_ssid"
+#define NVS_PASS_KEY "wifi_pass"
 
 String CFG_WIFI_SSID;
 String CFG_WIFI_PASS;
-
-void writeString(int addr, const String& data) {
-    for (size_t i = 0; i < data.length(); i++) {
-        EEPROM.write(addr + i, data[i]);
-    }
-    EEPROM.commit();
-}
-
-void readString(int addr, String& data, size_t maxLength) {
-    data = "";  // Limpa a string antes de ler
-
-    for (size_t i = 0; i < maxLength; i++) {
-        char charRead = EEPROM.read(addr + i);
-        if (charRead == '\0') {
-            break;  // Encerra se encontrar o caractere nulo
-        }
-        data += charRead;
-    }
-    
-    data.trim();  // Remove qualquer espaço em branco no final da string
-}
-
 
 String inputText() {
     String data = "> ";
@@ -71,7 +48,6 @@ String inputText() {
                     data.remove(0, 2);
                     M5Cardputer.Display.println(data);
                     return data;
-                    
                 }
 
                 M5Cardputer.Display.fillRect(0, M5Cardputer.Display.height() - 28, M5Cardputer.Display.width(), 25, BLACK);
@@ -92,69 +68,66 @@ void setup() {
     CFG_WIFI_SSID = "";
     CFG_WIFI_PASS = "";
 
-    // Lê os valores da EEPROM e inicializa as variáveis
-    readString(SSID_ADDR, CFG_WIFI_SSID,32);
-    readString(PASS_ADDR, CFG_WIFI_PASS,10);
+    Preferences preferences;
+    preferences.begin("wifi_settings", false);
+    CFG_WIFI_SSID = preferences.getString(NVS_SSID_KEY, "");
+    CFG_WIFI_PASS = preferences.getString(NVS_PASS_KEY, "");
+    preferences.end();
 
-  // Se desejar, pode exibir os valores na tela
-  //M5Cardputer.Display.setTextColor(PURPLE);
-  WiFi.disconnect();
-  WiFi.softAPdisconnect(true);
+    WiFi.disconnect();
+    WiFi.softAPdisconnect(true);
+    WiFi.begin(CFG_WIFI_SSID.c_str(), CFG_WIFI_PASS.c_str());
 
-  M5Cardputer.Display.drawString("Conectando no Wifi.", 10, 1);
-  M5Cardputer.Display.drawString("SSID: " + CFG_WIFI_SSID, 10, 15);
-  M5Cardputer.Display.drawString("Senha: " + CFG_WIFI_PASS, 10, 30);
-  WiFi.begin(CFG_WIFI_SSID, CFG_WIFI_PASS);
-
-  // try forever but not too much
-  int tm = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    M5Cardputer.Display.print(".");
-    delay(100);
-    if (M5Cardputer.Keyboard.isKeyPressed('m') || tm++ <30) {
-      break;
-    }
-  }
-  if (WiFi.status() == WL_CONNECTED) {
-  M5Cardputer.Display.println("");
-  M5Cardputer.Display.println("WiFi conectada.");
-  M5Cardputer.Display.println(WiFi.SSID());
-  M5Cardputer.Display.println("Endereço de IP: ");
-  M5Cardputer.Display.println(WiFi.localIP());
-  M5Cardputer.Display.println("Potencia: ");
-  M5Cardputer.Display.println(WiFi.RSSI());
-  M5Cardputer.Speaker.tone(1000, 100);
-  delay(2000);
-  } else { 
-    M5Cardputer.Display.clear();
-    M5Cardputer.Display.drawString("Configuracao de WiFi",1,1);
-    M5Cardputer.Display.setTextColor(GREEN);
-    M5Cardputer.Display.drawString("Digite o SSID:", 1, 15);
-    CFG_WIFI_SSID = inputText();
-    M5Cardputer.Display.clear();
-    M5Cardputer.Display.drawString("SSID: " + CFG_WIFI_SSID, 1, 15);
-    M5Cardputer.Display.setTextColor(YELLOW);
-    M5Cardputer.Display.drawString("Digite a senha:", 1, 32);
-    CFG_WIFI_PASS = inputText();
-
-    // Grava os valores na EEPROM
-    writeString(SSID_ADDR, CFG_WIFI_SSID);
-    writeString(PASS_ADDR, CFG_WIFI_PASS);
-    M5Cardputer.Display.clear();
-    M5Cardputer.Display.drawString("SSID: " + CFG_WIFI_SSID, 1, 15);
-    M5Cardputer.Display.drawString("Senha: " + CFG_WIFI_PASS, 1, 32);
-    M5Cardputer.Display.drawString("SSID e Senha gravados.", 1, 60);
-    WiFi.begin(CFG_WIFI_SSID, CFG_WIFI_PASS);
-      while (WiFi.status() != WL_CONNECTED) {
-      M5Cardputer.Display.print(".");
-      delay(100);
-        if (M5Cardputer.Keyboard.isKeyPressed('m') || tm++ <30) {
-          break;
-        }
+    int tm = 0;
+    while (tm++ < 60 && WiFi.status() != WL_CONNECTED) {
+        M5Cardputer.Display.print(".");
+        delay(100);
+      if (M5Cardputer.Keyboard.isKeyPressed('´')) {
+        break;
       }
-  }
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+    M5Cardputer.Display.clear();
+    M5Cardputer.Display.setCursor(0, 0);
+    M5Cardputer.Display.println("WiFi conectada.");
+    M5Cardputer.Display.println("SSID: " + WiFi.SSID());
+    M5Cardputer.Display.println("IP: " + WiFi.localIP());
+    M5Cardputer.Display.println("Power: " + WiFi.RSSI());
+    M5Cardputer.Speaker.tone(800, 100);
+    delay(200);
+    } else {
+        M5Cardputer.Display.clear();
+        M5Cardputer.Display.drawString("Configuracao de WiFi", 1, 1);
+        M5Cardputer.Display.drawString("Digite o SSID:", 1, 15);
+        CFG_WIFI_SSID = inputText();
+        M5Cardputer.Display.clear();
+        M5Cardputer.Display.drawString("SSID: " + CFG_WIFI_SSID, 1, 15);
+        M5Cardputer.Display.drawString("Digite a senha:", 1, 32);
+        CFG_WIFI_PASS = inputText();
+
+        Preferences preferences;
+        preferences.begin("wifi_settings", false);
+        preferences.putString(NVS_SSID_KEY, CFG_WIFI_SSID);
+        preferences.putString(NVS_PASS_KEY, CFG_WIFI_PASS);
+        preferences.end();
+
+        M5Cardputer.Display.clear();
+        M5Cardputer.Display.drawString("SSID: " + CFG_WIFI_SSID, 1, 15);
+        M5Cardputer.Display.drawString("Senha: " + CFG_WIFI_PASS, 1, 32);
+        M5Cardputer.Display.drawString("SSID e Senha gravados.", 1, 60);
+        WiFi.begin(CFG_WIFI_SSID.c_str(), CFG_WIFI_PASS.c_str());
+
+        M5Cardputer.Display.clear();
+        M5Cardputer.Display.setCursor(1,1);
+        M5Cardputer.Display.drawString("WiFi conectada.", 35,1);
+        M5Cardputer.Display.drawString("SSID: " + WiFi.SSID(), 1,15);
+        M5Cardputer.Display.drawString("IP: " + WiFi.localIP(), 1,32);
+        M5Cardputer.Display.drawString("Power: " + WiFi.RSSI(), 1,60);
+        M5Cardputer.Speaker.tone(6000, 100);
+        delay(200);
+    }
 }
 
 void loop() {
-    //adicionar lógica aqui.
+    // adicionar lógica aqui.
 }
